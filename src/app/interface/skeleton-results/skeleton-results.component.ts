4c45e73eeef2b355eab47bf2ac81cd4426b02ccd
J. Trpka <jtrpka0912@gmail.com>
2022-09-03 15:53:33 -0400

    Generated and created the skeleton component.

---
+++ src/app/interface/skeleton-results/skeleton-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-results',
+  templateUrl: './skeleton-results.component.html',
+  styleUrls: ['./skeleton-results.component.scss']
+})
+export class SkeletonResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
