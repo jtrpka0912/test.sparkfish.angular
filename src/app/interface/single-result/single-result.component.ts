00af9c7f63e57019c0ceddabdd01c38c359f1fba
J. Trpka <jtrpka0912@gmail.com>
2022-09-03 12:26:00 -0400

    Generated single result component.

---
+++ src/app/interface/single-result/single-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-result',
+  templateUrl: './single-result.component.html',
+  styleUrls: ['./single-result.component.scss']
+})
+export class SingleResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
